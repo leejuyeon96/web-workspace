02c0ba2ea38d10152dde22afa96c186dfce5ce29
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-08-01 17:47:00 +0900

    Create HttpServletTest.java

---
+++ 01_GenericServlet/src/servlet/http/HttpServletTest.java
@@ -0,0 +1,28 @@
+package servlet.http;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HttpServletTest
+ */
+public class HttpServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		PrintWriter 
+	}
+
+}
+
+
+
+
+
+
