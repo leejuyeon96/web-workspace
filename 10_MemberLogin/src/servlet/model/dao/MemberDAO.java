8aa4b939ef565b6536383b2a84444c843bed5f5a
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-08-08 17:41:46 +0900

    Update MemberDAO.java

--- 10_MemberLogin/src/servlet/model/dao/MemberDAO.java
+++ 10_MemberLogin/src/servlet/model/dao/MemberDAO.java
@@ -12,14 +12,31 @@ import servlet.model.vo.MemberDTO;
 
 public class MemberDAO implements MemberDAOTemplate {
 	
-	public MemberDAO() {
+	// 싱글톤 패턴 - 클래스의 객체가 항상 하나만 존재하도록
+	/*
+	 * DAO를 반복적으로 생성하고 해제하는 것은 비효율적
+	 * 객체지향적 설계! 싱글톤 패턴은 객체지향적 설계 원칙을 준수 -> 중앙에서 처리!
+	 * 주의할 점은 싱글톤은 전역 상태를 가질 수 있으므로 오남용하면 애플리케이션의 복잡성이 증가
+	 * */
+	private static MemberDAO dao = new MemberDAO();
+	private MemberDAO() {
 		try {
 			Class.forName(ServerInfo.DRIVER_NAME);
-		} catch (ClassNotFoundException e) {
-			
-			e.printStackTrace();
-		}
+		} catch (ClassNotFoundException e) {}
 	}
+	public static MemberDAO getInstance() {
+		return dao;
+	}
+	
+	
+//	public MemberDAO() {
+//		try {
+//			Class.forName(ServerInfo.DRIVER_NAME);
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//	}
+
 	@Override
 	public Connection getConnection() throws SQLException {
 		Connection conn = DriverManager.getConnection(ServerInfo.URL, ServerInfo.USER, ServerInfo.PASSWORD);
@@ -30,22 +47,19 @@ public class MemberDAO implements MemberDAOTemplate {
 	public void closeAll(PreparedStatement ps, Connection conn) throws SQLException {
 		ps.close();
 		conn.close();
-	
-		
 	}
 
 	@Override
 	public void closeAll(ResultSet rs, PreparedStatement ps, Connection conn) throws SQLException {
 		rs.close();
-		closeAll(ps,conn);
-		
+		closeAll(ps, conn);
 	}
 
 	@Override
 	public void registerMember(MemberDTO dto) throws SQLException {
 		Connection conn = getConnection();
 		
-		String query = "INSERT INTO member(id, password, name, address) VALUES (?,?,?,?)";
+		String query = "INSERT INTO member(id, password, name, address) VALUES(?, ?, ?, ?)";
 		PreparedStatement ps = conn.prepareStatement(query);
 		
 		ps.setString(1, dto.getId());
@@ -56,13 +70,14 @@ public class MemberDAO implements MemberDAOTemplate {
 		ps.executeUpdate();
 		
 		closeAll(ps, conn);
+		
 	}
 
 	@Override
 	public MemberDTO login(String id, String password) throws SQLException {
 		Connection conn = getConnection();
 		
-		String query = "SELECT* FROM member WHERE id=? AND password=?";
+		String query = "SELECT * FROM member WHERE id=? AND password=?";
 		PreparedStatement ps = conn.prepareStatement(query);
 		
 		ps.setString(1, id);
@@ -71,7 +86,7 @@ public class MemberDAO implements MemberDAOTemplate {
 		ResultSet rs = ps.executeQuery();
 		MemberDTO dto = null;
 		if(rs.next()) {
-			dto=new MemberDTO();
+			dto = new MemberDTO();
 			dto.setId(rs.getString("id"));
 			dto.setPassword(rs.getString("password"));
 			dto.setName(rs.getString("name"));
@@ -83,33 +98,64 @@ public class MemberDAO implements MemberDAOTemplate {
 
 	@Override
 	public MemberDTO findByIdMember(String id) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = getConnection();
+		
+		String query = "SELECT * FROM member WHERE id=?";
+		PreparedStatement ps = conn.prepareStatement(query);
+		
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		MemberDTO dto = null;
+		if(rs.next()) {
+			dto = new MemberDTO();
+			dto.setId(rs.getString("id"));
+			dto.setPassword(rs.getString("password"));
+			dto.setName(rs.getString("name"));
+			dto.setAddress(rs.getString("address"));
+		}
+		closeAll(rs, ps, conn);
+		return dto;
 	}
 
 	@Override
 	public ArrayList<MemberDTO> showAllMember() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = getConnection();
+		
+		String query = "SELECT * FROM member";
+		PreparedStatement ps = conn.prepareStatement(query);
+		
+		ResultSet rs = ps.executeQuery();
+		ArrayList<MemberDTO> list = new ArrayList<>();
+		while(rs.next()) {
+			MemberDTO dto = new MemberDTO();
+			dto.setId(rs.getString("id"));
+			dto.setPassword(rs.getString("password"));
+			dto.setName(rs.getString("name"));
+			dto.setAddress(rs.getString("address"));
+			list.add(dto);
+		}
+		closeAll(rs, ps, conn);
+		return list;
 	}
-	public static void main(String[]args) {
+	
+	public static void main(String[] args) {
 		MemberDAO dao = new MemberDAO();
 		
 		MemberDTO dto = new MemberDTO();
 		dto.setId("user1");
 		dto.setPassword("user1");
 		dto.setName("김미경");
-		dto.setAddress("경기도 광주");
+		dto.setAddress("경기 광주");
 		
 		try {
-			dao.login("user1", "user1");
+			//dao.registerMember(dto);
+			
+			dto = dao.login("user1", "user1");
 			System.out.println("name : " + dto.getName());
 			System.out.println("address : " + dto.getAddress());
 			
-		} catch (SQLException e) {
-			
-			
-		}
+		} catch (SQLException e) {}
 	}
-	
+
 }
