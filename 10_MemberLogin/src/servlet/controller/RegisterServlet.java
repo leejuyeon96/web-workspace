0fb174b74760d0e893977df4b680739628b5f6e8
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-08-08 17:41:44 +0900

    Update RegisterServlet.java

--- 10_MemberLogin/src/servlet/controller/RegisterServlet.java
+++ 10_MemberLogin/src/servlet/controller/RegisterServlet.java
@@ -12,48 +12,45 @@ import javax.servlet.http.HttpSession;
 import servlet.model.dao.MemberDAO;
 import servlet.model.vo.MemberDTO;
 
-
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-   
-    private 
        
-
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	//1. 폼값 가져온다.
-    	String id = request.getParameter("id");
+		// 1. 폼값 가져온다.
+		String id = request.getParameter("id");
 		String password = request.getParameter("password");
 		String name = request.getParameter("name");
 		String address = request.getParameter("address");
 		
-		//2. 객체 생성
+		// 2. 객체 생성 - 폼 값 담기
 		MemberDTO dto = new MemberDTO();
 		dto.setId(id);
 		dto.setPassword(password);
 		dto.setName(name);
 		dto.setAddress(address);
 		
-		//3. DAO와 연결
-		MemberDAO dao = new MemberDAO();
 		try {
-		dao.registerMember(dto);
-    
-		//4. 데이터 바인딩 - session
-		HttpSession session = request.getSession();
-		session.setAttribute("dto", dto);
-		
-		//5.네비게이션
-		response.sendRedirect("AllMemberServlet");
-		
-		}catch (SQLException e) {
+			// 3. DAO와 연결
+//			MemberDAO dao = new MemberDAO();
+			MemberDAO.getInstance().registerMember(dto);
+			
+			// 4. 데이터 바인딩 - session
+			HttpSession session = request.getSession();
+			session.setAttribute("dto", dto);
+			
+			// 5. 네비게이션
+			response.sendRedirect("AllMemberServlet");
+			
+		} catch (SQLException e) {
 			
 			System.out.println("회원가입 실패!");
 			response.sendRedirect("../index.jsp");
+			
 		}
-    }
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
 
