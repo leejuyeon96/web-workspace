d80f33e58d8b5b83fe5a605d453b9b8f03ed7766
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-08-16 17:42:36 +0900

    Create SerialAjax.java

---
+++ 20_AjaxJquery/src/servlet/ajax/SerialAjax.java
@@ -0,0 +1,38 @@
+package servlet.ajax;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/SerialAjax")
+public class SerialAjax extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+    public SerialAjax() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		String name = request.getParameter("name");
+		String addr = request.getParameter("addr");
+		
+		PrintWriter out= response.getWriter();
+		out.print(addr+"에 사는"+ name + "님");
+		out.close();
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
