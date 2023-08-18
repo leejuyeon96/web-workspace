9a1530692e4207c971c26578658f77fb0d245d61
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-08-18 17:43:18 +0900

    13 Member

--- 13_MemberJSTL/src/controller/AllMemberServlet.java
+++ 13_MemberJSTL/src/controller/AllMemberServlet.java
@@ -5,25 +5,30 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import servlet.model.dao.MemberDAO;
+import servlet.model.service.MemberService;
 import servlet.model.vo.MemberDTO;
 
 /**
  * Servlet implementation class AllMemberServlet
  */
+@WebServlet("/member/all")
 public class AllMemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //		MemberDAO dao = new MemberDAO();
 		try {
-			ArrayList<MemberDTO> list = MemberDAO.getInstance().showAllMember();
+			ArrayList<MemberDTO> list = new MemberService().showAllMember();
 			request.setAttribute("list", list);
-			request.getRequestDispatcher("views/allShow.jsp").forward(request, response);
+			request.getRequestDispatcher("/views/allShow.jsp").forward(request, response);
 		} catch (SQLException e) {}
 	}
 
